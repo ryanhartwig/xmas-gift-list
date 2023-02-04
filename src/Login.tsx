8213b30f145ae00486397d243ad42479293ff167
Ryan1431 <ryan1431@gmail.com>
2023-02-04 15:16:28 -0500

    Contrast adjustments

--- src/Login.tsx
+++ src/Login.tsx
@@ -15,7 +15,7 @@ export const Login = ({setSelectedUser}: LoginProps) => {
 
   return (
     <div className="login">
-      <h1>{w.map((c, i) => <span key={`${c}-${i}`} style={{color: i % 2 === 0 ? 'red' : 'green'}}>{c}</span>)}</h1>
+      <h1>{w.map((c, i) => <span key={`${c}-${i}`} style={{color: i % 2 === 0 ? 'red' : 'green', fontWeight: 300}}>{c}</span>)}</h1>
       <div style={{position: 'relative'}}>
         <p>Now with <span style={{fontSize: '32px'}}>REAL-TIME</span> updates!11!!1</p>
         <img className='wow' referrerPolicy='no-referrer' src="https://static.wikia.nocookie.net/jerma-lore/images/7/78/BIG_WOW.png/revision/latest?cb=20200903171222" alt='thing'></img>
