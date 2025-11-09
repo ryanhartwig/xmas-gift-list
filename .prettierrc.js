6c9609ce63027ab3b30fc047e710e3b8c3c1a025
Andrew Hartwig <andrew.hartwig1@gmail.com>
2025-11-09 15:34:22 -0500

    chore: add code formatting

---
+++ .prettierrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  // Print width - line length that the printer will wrap on
+  printWidth: 80,
+  
+  // Tab width - number of spaces per indentation level
+  tabWidth: 2,
+  
+  // Use tabs instead of spaces
+  useTabs: false,
+  
+  // Semicolons at the end of statements
+  semi: true,
+  
+  // Use single quotes instead of double quotes
+  singleQuote: true,
+  
+  // Quote properties in objects only when necessary
+  quoteProps: 'as-needed',
+  
+  // Use single quotes in JSX
+  jsxSingleQuote: true,
+  
+  // Trailing commas where valid in ES5 (objects, arrays, etc.)
+  trailingComma: 'es5',
+  
+  // Spaces between brackets in object literals
+  bracketSpacing: true,
+  
+  // Put the > of a multi-line JSX element at the end of the last line
+  bracketSameLine: false,
+  
+  // Include parentheses around a sole arrow function parameter
+  arrowParens: 'avoid',
+  
+  // Format only files that have a pragma at the top
+  requirePragma: false,
+  
+  // Insert pragma at the top of files
+  insertPragma: false,
+  
+  // Prose wrap - how to wrap prose
+  proseWrap: 'preserve',
+  
+  // HTML whitespace sensitivity
+  htmlWhitespaceSensitivity: 'css',
+  
+  // End of line character
+  endOfLine: 'lf',
+  
+  // Control whether Prettier formats quoted code embedded in the file
+  embeddedLanguageFormatting: 'auto',
+  
+  // Enforce single attribute per line in HTML, Vue and JSX
+  singleAttributePerLine: false,
+};
